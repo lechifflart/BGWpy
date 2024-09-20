7648dcf66512bab8ed1d66df0a31730e515880c7
DaanHolleman <dholleman123@gmail.com>
2024-09-20 09:33:47 +0200

    Phtask implementation

--- BGWpy/QE/phtask.py
+++ BGWpy/QE/phtask.py
@@ -105,7 +105,7 @@ class QePhInput(Writable):
         return S
 
 
-# Base structure copied from QE2BGW task.
+# Daan; Base structure copied from QE2BGW task.
 class QePhTask(BaseQePhTask):
     """Phonon calculation."""
 
@@ -129,46 +129,22 @@ class QePhTask(BaseQePhTask):
         (All mandatory unless specified otherwise)
 
         prefix : str
-            Prefix required by QE as a rootname.
-        ngkpt : list(3), float
-            K-points grid. Number of k-points along each primitive vector
-            of the reciprocal lattice.
-        kshift : list(3), float, optional
-            Relative shift of the k-points grid along each direction,
-            as a fraction of the smallest division along that direction.
-        qshift : list(3), float, optional
-            Absolute shift of the k-points grid along each direction.
-        wfn_fname : str ('wfn.cplx'), optional
-            Name of the output wavefunction file.
-        rhog_flag : bool (False), optional
-            If True, will activacte the output of the density and vxc.
-        rho_fname : str, optional
-            Name of the ouput density file.
-            If provided, will activacte the output of the density and vxc.
-        nbnd : int, optional
-            Number of bands for which vxc should be computed.
-            Only if output of the density and vxc is active.
-
-
+            Prepended to input/output filenames; must be the same
+            used in the calculation of unperturbed system.
+        
         Properties
         ----------
-
-        wfn_fname : str
-            Path to the wavefunction file produced.
-        rho_fname : str
-            Path to the density file produced.
-        vxc_dat_fname : str
-            Path to the vxc.dat file produced.
+        
         """
-
+        
         kwargs.setdefault('runscript_fname', 'ph.run.sh')
-
+        
         super(QePhTask, self).__init__(dirname, **kwargs)
-
+        
         self.title_line = kwargs.pop('title_line')
         
         # Maybe let the defaults be handled by PW2BGWInput
-
+        
         # Input file
         defaults = dict(
             title_line  = self.title_line,
@@ -179,51 +155,19 @@ class QePhTask(BaseQePhTask):
             variables[key] = kwargs.get(key, value)
         
         self.input = QePhInput(prefix=self.prefix, **variables)
-
-        # Have to make sure the properties are set correctly.
-        if 'wfn_fname' in kwargs:
-            self.wfn_fname = kwargs['wfn_fname']
-        elif 'wfng_file' in kwargs:
-            self.wfn_fname = kwargs['wfng_file']
-        if 'rho_fname' in kwargs:
-            self.rho_fname = kwargs['rho_fname']
-        if 'vxc_dat_fname' in kwargs:
-            self.vxc_dat_fname = kwargs['vxc_dat_fname']
-
         self.input.fname = self._input_fname
 
         # Run script
-        self.runscript['PW2BGW'] = 'pw2bgw.x'
-        self.runscript.append('$MPIRUN $PW2BGW $PWFLAGS -in {} &> {}'.format(
+        self.runscript['PH'] = 'ph.x'
+        self.runscript.append('$MPIRUN $PH $PHFLAGS -in {} &> {}'.format(
                               self._input_fname, self._output_fname))
-
-    _wfn_fname = 'wfn.cplx'
-    @property
-    def wfn_fname(self):
-        return os.path.join(self.dirname, self._wfn_fname)
     
-    @wfn_fname.setter
-    def wfn_fname(self, value):
-        self._wfn_fname = value
-        self.input['wfng_file'] = value
+    # _wfn_fname = 'wfn.cplx'
+    # @property
+    # def wfn_fname(self):
+    #     return os.path.join(self.dirname, self._wfn_fname)
     
-    _rho_fname = 'rho.real'
-    @property
-    def rho_fname(self):
-        return os.path.join(self.dirname, self._rho_fname)
-    
-    @rho_fname.setter
-    def rho_fname(self, value):
-        self._rho_fname = value
-        self.input['rhog_file'] = value
-    
-    _vxc_dat_fname = 'vxc.dat'
-    @property
-    def vxc_dat_fname(self):
-        return os.path.join(self.dirname, self._vxc_dat_fname)
-    
-    @vxc_dat_fname.setter
-    def vxc_dat_fname(self, value):
-        self._vxc_dat_fname = value
-        self.input['vxc_file'] = value
-
+    # @wfn_fname.setter
+    # def wfn_fname(self, value):
+    #     self._wfn_fname = value
+    #     self.input['wfng_file'] = value
