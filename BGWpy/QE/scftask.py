d0299e39cf2b3b2828e058002a6017b1ee1fd07f
DaanHolleman <dholleman123@gmail.com>
2024-10-10 10:08:39 +0200

    Update scftask.py

--- BGWpy/QE/scftask.py
+++ BGWpy/QE/scftask.py
@@ -74,10 +74,8 @@ class QeScfTask(QeTask):
 
         super(QeScfTask, self).__init__(dirname, **kwargs)
 
-        # kpts, wtks = self.get_kpts(**kwargs)
-        kpts = [2,2,2]
-        wtks = [1,1,1]
-
+        kpts, wtks = self.get_kpts(**kwargs)
+        
         # Input file
         self.input = get_scf_input(
             self.prefix,
