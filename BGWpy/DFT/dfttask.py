dd3a0357dd532dc6a2dd7b6a2d485fc8f0ae00e3
DaanHolleman <dholleman123@gmail.com>
2024-10-15 15:58:27 +0200

    Remove debug

--- BGWpy/DFT/dfttask.py
+++ BGWpy/DFT/dfttask.py
@@ -93,7 +93,6 @@ class DFTTask(MPITask):
                 kpts, wtks = self.kgridtask.get_kpt_grid_nosym()
         else:
             kpts, wtks = kwargs['kpts'], kwargs['wtks']
-        print(kpts, wtks)
         return kpts, wtks
 
     def check_pseudos(self):
