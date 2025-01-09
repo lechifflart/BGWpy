eae06ec0ca2fd02202b7c8e1ca6b3fd813f0d6b5
Daan  Holleman <dholleman@int5.local.snellius.surf.nl>
2025-01-09 10:16:38 +0100

    keyword fildvscf

--- BGWpy/QE/phtask.py
+++ BGWpy/QE/phtask.py
@@ -58,7 +58,7 @@ class QePhInput(Writable):
 
         # Other inputph
         keys = ['qplot', 'ldisp', 'nq1','nq2','nq3', 
-                'asr', 'nogg', 'tr2_ph', 'fildyn', 'reduce_io',
+                'asr', 'nogg', 'tr2_ph', 'fildvscf', 'fildyn', 'reduce_io',
                 'electron_phonon', 'verbosity']
         for key in keys:
             if key in kwargs:
